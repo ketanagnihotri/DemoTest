1dd364b46c856b50b88adf63358ed8838b46976f
Ketan Agnihotri <kagnihotri@internal.tavisca.com>
2016-04-28 12:41:30 +0530

    test

--- DemoTests/DemoTestCases.cs
+++ DemoTests/DemoTestCases.cs
@@ -18,7 +18,7 @@ namespace DemoTests
         [Fact]
         public void FailingTest()
         {
-            Assert.Equal(5, Add(2, 2));
+            Assert.Equal(5, Add(2, 3));
         }
 
         int Add(int x, int y)
